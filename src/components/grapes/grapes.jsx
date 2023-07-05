b038bbf16027ab8da0b3d1c79a331cc64196c79a
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-07-05 11:13:34 +0100

    created the layout and organised the folder structure

---
+++ src/components/grapes/grapes.jsx
@@ -0,0 +1,9 @@
+const Grapes = () => {
+  return (
+    <div>
+      <p>Hello World!!</p>
+    </div>
+  );
+};
+
+export default Grapes;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Grapes from "@/components/grapes/grapes";
+import React from "react";
+
+export default function Index() {
+  return (
+    <div>
+      <Grapes />
+    </div>
+  );
+}
