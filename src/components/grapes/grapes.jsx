97e3f9c6b4418a1b6333237e942cc587ae5c8422
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-07-14 15:03:34 +0100

    utility mehods and timeline

--- src/components/grapes/grapes.jsx
+++ src/components/grapes/grapes.jsx
@@ -10,13 +10,47 @@ const Grapes = () => {
   const txt = useRef();
   const btn = useRef();
   const allImages = useRef();
+  const tween = useRef();
+  const tl = gsap.timeline();
 
+  /*
+    UTILITY METHODS
+    tween.current = gsap.to(imgTwo.current, {
+      y: 200,
+      duration: 5,
+      paused: true,
+    });
+    setTimeout(() => {
+      tween.current.seek(2.5);
+      tween.current.play();
+    }, 2000);
+    */
+
+  const handleClick = () => {
+    /*TIMELINES*/
+    allImages.current.classList.remove("invisible");
+    tl.from(imgOne.current, {
+      autoAlpha: 0,
+      y: -50,
+      duration: 1,
+    });
+    tl.from(imgTwo.current, {
+      autoAlpha: 0,
+      y: -50,
+      duration: 1,
+    });
+    tl.from(imgThree.current, {
+      autoAlpha: 0,
+      y: -50,
+      duration: 1,
+    });
+  };
   useEffect(() => {}, []);
 
   return (
     <div>
       <div className="bg-green-200 w-screen h-screen " ref={containerImages}>
-        <div className="flex" ref={allImages}>
+        <div className="flex invisible " ref={allImages}>
           <div className=" max-w-full max-h-full">
             <img
               src="./cat.svg"
@@ -44,46 +78,26 @@ const Grapes = () => {
         </div>
         <div className="flex justify-center mt-20">
           <button
-            onClick={() => {
-              gsap.registerEffect({
-                name: "imgAnimation",
-                effect: (targets, config) => {
-                  return gsap.to(targets, {
-                    duration: config.duration,
-                    y: 200,
-                    scale: 1.4,
-                    rotation: 360,
-                  });
-                },
-                defaults: {
-                  duration: 2,
-                },
-              });
-              gsap.effects.imgAnimation(imgOne.current, {
-                duration: 5,
-              });
-            }}
+            onClick={handleClick}
             className="rounded bg-blue-700 px-5 text-white"
             ref={btn}
           >
             Validate
           </button>
         </div>
+        <h1
+          className="text-center text-black leading-6 mt-12 text-base sm:text-lg lg:text-xl xl:text-2xl pt-0 pb-2.5 md:mt-50"
+          ref={title}
+        >
+          Welcome to the sandbox.
+        </h1>
+        <p
+          className="text-center text-black leading-6 mt-2.5 text-xs sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl pt-0 pb-2.5"
+          ref={txt}
+        >
+          Let's play with Greensock.
+        </p>
       </div>
-
-      <h1
-        className="text-center text-black leading-6 mt-12 text-base sm:text-lg lg:text-xl xl:text-2xl pt-0 pb-2.5 md:mt-50"
-        ref={title}
-      >
-        Welcome to the sandbox.
-      </h1>
-      <p
-        className="text-center text-black leading-6 mt-2.5 text-xs sm:text-base lg:text-lg xl:text-xl 2xl:text-2xl pt-0 pb-2.5"
-        ref={txt}
-      >
-        Let's play with Greensock.
-      </p>
-      <div className="flex justify-center bg-blue-700"></div>
     </div>
   );
 };
